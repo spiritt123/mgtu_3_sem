21cf4a0abcbb2088e2c408821baefaea62e8253a
spiritt123 <medaleks2001@yandex.ru>
2020-09-29 19:21:23 +0300

    mini update

--- IP/lab5/main.rb
+++ IP/lab5/main.rb
@@ -1,7 +1,15 @@
-x = gets.to_i 
-y = (Math.log10((x * x)/(x - 2))) * Math.exp(x);
 
 
-puts y
-
+def my_fun(x)
+  if (x == 0) 
+    return -Float::INFINITY
+  end
+  if (x == 2)
+    return Float::INFINITY
+  end
+  if ((x * x)/(x - 2) < 0)
+    return Float::NAN
+  end
+  (Math.log10((x * x)/(x - 2))) * Math.exp(x);
+end
 
